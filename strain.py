85daa92f6c6be1039a416ee98a318f1a06a39d0e
Sahil Pattni <sahilpattni97@gmail.com>
2023-12-17 20:36:42 -0500

    Completed Strain class implementation.

--- strain.py
+++ strain.py
@@ -1,13 +1,21 @@
+import pickle
+import os
+
+from utils.custom_logger import Logger
+
+logger = Logger().get_logger()
+
+
 class Strain:
     def __init__(
         self,
         sku: str,
-        name: str = "(name not assigned)",
+        name: str = None,
         list_price: float = None,
         display_price: float = None,
         quantity: int = None,
         promised_quantity: int = None,
-        url: str = "(url not assigned)",
+        url: str = None,
     ):
         """
         :param sku: The SKU of the strain
@@ -26,12 +34,51 @@ class Strain:
         self.__promised_quantity = promised_quantity
         self.__url = url
 
+    @staticmethod
+    def load(directory: str, filename: str):
+        """
+        Loads the strain object from a pickle file.
+        :param directory: The directory to load the file from.
+        :param filename: The name of the file to load.
+        :return: The strain object.
+        """
+        filepath: str = os.path.join(directory, filename)
+        with open(filepath, "rb") as f:
+            return pickle.load(f)
+
+    def save(self, directory: str) -> None:
+        """
+        Saves the strain object to a pickle file.
+        :param directory: The directory to save the file to.
+        """
+        # Base case: if directory does not exist, create it
+        if not os.path.exists(directory):
+            logger.debug(f"Directory `{directory}` does not exist. Creating it...")
+            os.makedirs(directory)
+        filename: str = f"{self.name}.weed"
+        filepath: str = os.path.join(directory, filename)
+        with open(filepath, "wb") as f:
+            pickle.dump(self, f)
+
+    @property
+    def is_processed(self) -> bool:
+        """
+        :return: Returns true if all the instance variables have been assigned.
+        """
+        return not any(x is None for x in self.__dict__.values())
+
     @property
     def sku(self):
+        """
+        :return: The SQDC SKU of the strain.
+        """
         return self.__sku
 
     @property
     def product_id(self):
+        """
+        :return: The SKU of the strain with a "-P" appended to the end. Used as a unique key.
+        """
         return self.__sku + "-P"
 
     @property
